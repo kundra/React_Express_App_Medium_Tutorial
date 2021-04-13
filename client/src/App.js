e17e21e406af860c513092b4b6e9f93f7d7d447a
kundra <misskundra1@gmail.com>
2021-04-14 02:47:05 +0530

    Setting up routes, two forms and installing material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import NewOrder from "./Forms/NewOrder";
+import StockAddition from "./Forms/StockAddition";
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = {
+            apiResponse: "",
+            value: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.fullName);
+        event.preventDefault();
     }
 
     callAPI() {
@@ -24,9 +37,21 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">Murphy Sales & Stock Tracker</h1>
                 </header>
                 <p className="App-intro">{this.state.apiResponse}</p>
+
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/newOrder">
+                            <NewOrder/>
+                        </Route>
+                        <Route path="/stockAddition">
+                            <StockAddition/>
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+
             </div>
         );
     }
